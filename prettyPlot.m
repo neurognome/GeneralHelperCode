1612b8ba8b48a1e2d0920b7f0a574669847bf644
kevinksit <kevinkamansit@gmail.com>
2019-07-18 08:13:43 -0700

    Added new helper function, prettyPlot.m

---
+++ prettyPlot.m
@@ -0,0 +1,14 @@
+function h = prettyPlot(h)
+% Written 18Jul2019 KS
+
+% A quick function just to make plots the way I like them for publications. Saves some work in illustrator
+% You can either supply an axes for it to work on, or it'll just take the current one 
+
+
+if nargin == 0
+    h = gca;
+end
+
+set(h,'box','off');
+set(h,'TickDir','out');
+set(h,'LineWidth',2);
