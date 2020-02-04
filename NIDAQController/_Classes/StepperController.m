30ef708edf6ebb3f1c707dcac601d2bdae88d0a7
kevinksit <kevinkamansit@gmail.com>
2020-02-03 18:37:11 -0800

    Got the stepper controller working better with queued data

--- NIDAQController/_Classes/StepperController.m
+++ NIDAQController/_Classes/StepperController.m
@@ -1,6 +1,7 @@
 classdef StepperController < NIDAQController
     properties (Constant = true)
         STEPS_PER_REV = 200;
+        clockFreq = 10000;
     end
     
     properties
@@ -9,18 +10,27 @@ classdef StepperController < NIDAQController
         microstep_lines 
         
         microstep_value
+
+        clock_session
     end
     
     methods
         function obj = StepperController(lines)
-            
+
             obj = obj@NIDAQController(10000); % Call superclass constructor
-            
+            % From https://www.mathworks.com/help/daq/acquire-digital-data-using-a-counter-output-channel-as-external-clock.html
+            obj.clock_session = daq.createSession('ni');
+            ch1 = addCounterOutputChannel(obj.clock_session,'Dev1',0,'PulseGeneration');
+            clkTerminal = ch1.Terminal;
+            ch1.Frequency = obj.clockFreq;
+            obj.clock_session.IsContinuous = true;
+
+
             % Assign lines
             if nargin < 1 || isempty(lines)
                 lines = obj.inputPorts({'step', 'direction', 'microstep1', 'microstep2', 'microstep3'});
             end
-             
+
             obj.step_line = obj.addDigitalOutput(lines{1});
             obj.dir_line = obj.addDigitalOutput(lines{2});
             for i_line = 1:3
@@ -28,6 +38,19 @@ classdef StepperController < NIDAQController
             end
             
             obj.output = zeros(1, length(lines));
+
+
+            obj.session.addClockConnection('External',['Dev1/' clkTerminal], 'ScanClock');
+            obj.clock_session.startBackground();
+            
+            for i = 1:10 
+                if obj.clock_session.IsRunning
+                    break;
+                else
+                    pause(0.1);
+                end
+            end
+
         end
         
         function setMicrostepAmount(obj, microstep_amount)
@@ -39,26 +62,26 @@ classdef StepperController < NIDAQController
                 microstep_amount = choices{val};
             end
             switch microstep_amount
-                case 'Full'
-                    obj.digitalWrite(obj.microstep_lines(1), 0);
-                    obj.digitalWrite(obj.microstep_lines(2), 0);
-                    obj.digitalWrite(obj.microstep_lines(3), 0);
-                case 'Half'
-                    obj.digitalWrite(obj.microstep_lines(1), 1);
-                    obj.digitalWrite(obj.microstep_lines(2), 0);
-                    obj.digitalWrite(obj.microstep_lines(3), 0);
-                case 'Quarter'
-                    obj.digitalWrite(obj.microstep_lines(1), 0);
-                    obj.digitalWrite(obj.microstep_lines(2), 1);
-                    obj.digitalWrite(obj.microstep_lines(3), 0);
-                case 'Eighth'
-                    obj.digitalWrite(obj.microstep_lines(1), 1);
-                    obj.digitalWrite(obj.microstep_lines(2), 1);
-                    obj.digitalWrite(obj.microstep_lines(3), 0);
-                case 'Sixteenth'
-                    obj.digitalWrite(obj.microstep_lines(1), 1);
-                    obj.digitalWrite(obj.microstep_lines(2), 1);
-                    obj.digitalWrite(obj.microstep_lines(3), 1);
+            case 'Full'
+                obj.digitalWrite(obj.microstep_lines(1), 0);
+                obj.digitalWrite(obj.microstep_lines(2), 0);
+                obj.digitalWrite(obj.microstep_lines(3), 0);
+            case 'Half'
+                obj.digitalWrite(obj.microstep_lines(1), 1);
+                obj.digitalWrite(obj.microstep_lines(2), 0);
+                obj.digitalWrite(obj.microstep_lines(3), 0);
+            case 'Quarter'
+                obj.digitalWrite(obj.microstep_lines(1), 0);
+                obj.digitalWrite(obj.microstep_lines(2), 1);
+                obj.digitalWrite(obj.microstep_lines(3), 0);
+            case 'Eighth'
+                obj.digitalWrite(obj.microstep_lines(1), 1);
+                obj.digitalWrite(obj.microstep_lines(2), 1);
+                obj.digitalWrite(obj.microstep_lines(3), 0);
+            case 'Sixteenth'
+                obj.digitalWrite(obj.microstep_lines(1), 1);
+                obj.digitalWrite(obj.microstep_lines(2), 1);
+                obj.digitalWrite(obj.microstep_lines(3), 1);
             end
             
             obj.microstep_value = microstep_amount;
@@ -66,19 +89,56 @@ classdef StepperController < NIDAQController
         
         function step(obj)
             obj.digitalWrite(obj.step_line, 1);
-            java.lang.Thread.sleep(0.001);
+            java.lang.Thread.sleep(0.5);
             obj.digitalWrite(obj.step_line, 0);
-            java.lang.Thread.sleep(0.001);
+            java.lang.Thread.sleep(0.5);
         end
         
         function setDirection(obj, direction)
             switch direction
-                case 'forward'
-                    obj.digitalWrite(obj.dir_line, 1);
-                case 'backward'
-                    obj.digitalWrite(obj.dir_line, 0);
+            case 'forward'
+                obj.digitalWrite(obj.dir_line, 1);
+            case 'backward'
+                obj.digitalWrite(obj.dir_line, 0);
             end
         end
+
+        function queue(obj, duration, speed)
+            % Add in checks for the maximum speeds given the fs
+            n_samples = round(duration .* obj.session.Rate);
+            n_revolutions = (speed/60) * duration; % rps
+            n_steps = n_revolutions * 200;
+            step_vec = round(linspace(1, n_samples-1, n_steps));
+            drive_vector = false(1, n_samples);
+
+            drive_vector(step_vec) = true; % Setting steps in the true portions...
+            obj.session.queueOutputData(cat(2, drive_vector', ones(n_samples, 1), zeros(n_samples, 3)));
+        end
+
+
+        function queueSteps(obj, n_steps, speed)
+            duration = n_steps * 1/((speed/60) * 200); % nsteps * rotations per second * 1/steps per rotation
+            n_samples = round(duration .* obj.session.Rate);
+            step_vec = round(linspace(1, n_samples-1, n_steps));
+            drive_vector = false(1, n_samples);
+            drive_vector(step_vec) = true;
+            obj.session.queueOutputData(cat(2, drive_vector', ones(n_samples, 1), zeros(n_samples, 3)));
+        end
+
+        function rotate(obj, angle, speed)
+            if nargin < 3 || isempty(speed)
+                speed = 30;
+            end
+            n_steps = round((angle/360) .* obj.STEPS_PER_REV);
+            disp(n_steps)
+            obj.queueSteps(n_steps, speed);
+            obj.drive();
+        end
+
+        function drive(obj)
+            obj.session.startBackground();
+        end
+
         function demo(obj)
             duration = 10;
             tic
@@ -98,14 +158,6 @@ classdef StepperController < NIDAQController
             end
             toc
         end
-        
-        % As we develop this class, there's going to need to be a lot of conversions to get from steps to time, etc, while
-        % taking into account the microstepping value (make sure to scale it properly).
-        
-        % We can figure this out!! the DELAY (converted from RPM) will give the # of steps per minute, therefore RPM.
-        % Each PULSE is a step, and we change the frequency of these pulses to get RPM!
-        
-        
+
     end
-    
 end
--- NIDAQController/nidaq_testing_script.m
+++ NIDAQController/nidaq_testing_script.m
@@ -12,8 +12,15 @@ t(1).setMicrostepAmount('Full')
 %t(2).setMicrostepAmount('Full')
 
 duration = 0.05;
-for ii = 1:20000
+for ii = 1:200
     t(1).step();    
     java.lang.Thread.sleep(0);
  %   t(2).step();
 end
+
+
+t.queue(5, 200)
+t.queue(5, 100);
+t.queue(5, 0);
+t.queue(60, 1);
+t.drive();
