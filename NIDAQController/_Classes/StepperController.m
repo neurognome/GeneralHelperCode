526799ed0363b97cc96f9d1775227d5e79eb8b65
kevinksit <kevinkamansit@gmail.com>
2020-02-26 19:58:00 -0800

    Cleaned up the StepperController

--- NIDAQController/_Classes/StepperController.m
+++ NIDAQController/_Classes/StepperController.m
@@ -36,29 +36,30 @@ classdef StepperController < NIDAQController
 			obj.clock.startClock();
 		end
 
-		function queue(obj, speed, input_type, value)
+        % I think we should rewrite this in the future, it's messy af
+		function output = queue(obj, speed, input_type, value)
 			if strcmp(input_type, 'steps') && (length(speed) ~= length(obj.motors) || length(value) ~= length(obj.motors))
 				%error('Input the same number of speed/values as motors');
                 value = repmat(value, 1, length(obj.motors));
             end
 
-            norm = min(speed); % idk why we need this right now, but it's to normalize the speeds to get proper scaling
+            speed = speed/min(speed); % Adjusting RPMs to scale properly?
 
             obj.checkSpeed(max(speed))
 
             switch input_type
             case 'steps'
+
                 for n = 1:length(obj.motors)
-                    n_steps(n) = (value(n) .* obj.aux_controller(n).getMicrostepScale() * speed(n))/norm;
+                    n_steps(n) = (value(n) .* obj.aux_controller(n).getMicrostepScale() * speed(n));
                 end
                 duration = obj.getDuration(n_steps(1), speed(1));
             case 'seconds'
                 duration = value;
                 for n = 1:length(obj.motors)
-                    n_steps = obj.getSteps(duration, speed(n) * obj.aux_controller(n).getMicrostepScale());
+                    n_steps(n) = obj.getSteps(duration, speed(n) * obj.aux_controller(n).getMicrostepScale());
                 end
             end
-
 
             % When steps are 0, then time is 0, get rid of these errors
             duration(isnan(duration)) = 0;
